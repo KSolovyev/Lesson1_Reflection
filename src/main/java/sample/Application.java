b3714c1d088ce21403f86c298a8cb533966d8c5c
Solovyev Konstantin <k.solovyev@my.com>
2017-02-11 02:29:16 +0300

    jetty server

---
+++ src/main/java/sample/Application.java
@@ -0,0 +1,21 @@
+package sample;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ * Created by Solovyev on 11/02/2017.
+ */
+public class Application {
+
+    public static final int PORT = 8081;
+
+    public static void main(String[] args) throws Exception {
+        final Server server = new Server(PORT);
+        final ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(servletContextHandler);
+        server.start();
+        server.join();
+    }
+}
